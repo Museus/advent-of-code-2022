1bd6e539499001dfa24a171064f33470095dbc53
Museus <museus@protonmail.com>
2022-12-04 19:35:22 -0500

    solution: day 1, part 2

--- day_1.py
+++ day_1.py
@@ -28,7 +28,22 @@ def get_elf_with_most_calories(calories_file_path: pathlib.Path):
 
     return sorted(elf_calories.items(), key=lambda x: x[1], reverse=True)[0]
 
-calories_file_path = pathlib.Path(CALORIES_FILE)
-result = get_elf_with_most_calories(calories_file_path)
+def get_three_elves_with_most_calories(calories_file_path: pathlib.Path):
+    food_items = load_calories_file(calories_file_path)
+    elf_calories = split_food_by_elf(food_items)
 
-print(f"Elf {result[0]} with {result[1]} calories.")
+    return sorted(elf_calories.items(), key=lambda x: x[1], reverse=True)[:3]
+
+calories_file_path = pathlib.Path(CALORIES_FILE)
+
+print("Top elf:")
+
+elf_with_most_calories = get_elf_with_most_calories(calories_file_path)
+print(f"  Elf {elf_with_most_calories[0]} with {elf_with_most_calories[1]} calories.")
+
+print("\nTop three elves:")
+three_elves_with_most_calories = get_three_elves_with_most_calories(CALORIES_FILE)
+for elf in three_elves_with_most_calories:
+    print(f"  Elf {elf[0]} with {elf[1]} calories.")
+
+print(f"\nTotal calories in top 3: {sum(calories for _, calories in three_elves_with_most_calories)}")
